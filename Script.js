7e9a5a035bf4d3696f33bd5f463363dcbe0dc604
Pooja <151643979+Poojadurgam10@users.noreply.github.com>
2024-03-10 15:28:47 +0530

    required to edit

--- Script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
-    alert('This is for Customers!');
-});
-const button1 = document.querySelector('button1');
-button1.addEventListener('click', () => {
-    alert('This is for Business!');
-});
