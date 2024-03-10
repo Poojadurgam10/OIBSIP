902fd8ec405e3f494ce7208c0468d1b89ef108e5
Pooja <151643979+Poojadurgam10@users.noreply.github.com>
2024-03-10 12:25:08 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,9 @@
+// script.js
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    alert('This is for Customers!');
+});
+const button1 = document.querySelector('button1');
+button1.addEventListener('click', () => {
+    alert('This is for Business!');
+});
